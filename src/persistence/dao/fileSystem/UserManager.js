8436c666ec02afafefac2de2e40586b18fb4ac0b
julianVillega <julianvillega9@gmail.com>
2024-09-04 16:33:49 -0300

    re-wrote UserManager.create

--- src/persistence/dao/fileSystem/UserManager.js
+++ src/persistence/dao/fileSystem/UserManager.js
@@ -9,24 +9,32 @@ export class UserManager {
     UserManager.#all.push(new User("user3@mail.com", "user3Password"));
   }
 
-  async create(email, password, photo, role) {
-    try {
-      // confirm the email is not used by othe users.
-      if (UserManager.#all.some((user) => user.email === email)) {
-        throw new Error(`The email address ${email} is already taken`);
-      }
+  create(email, password, photo, role) {
+    const result = new Promise((resolve, reject) => {
+      try {
+        // confirm the email is not empty
+        if (!email) {
+          reject(`No email was provided`);
+        }
 
-      // confirm the password is not empty
-      if (password.length === 0) {
-        throw new Error(`No password was provided`);
-      }
+        // confirm the email is not used by othe users.
+        if (UserManager.#all.some((user) => user.email === email)) {
+          reject(`The email address ${email} is already taken`);
+        }
 
-      //create the new user
-      const user = new User(email, password, photo, role);
-      UserManager.#all.push(user);
-      return user;
-    } catch (error) {
-      console.log(`Error at UserManager.create(): ${error}`);
-    }
+        // confirm the password is not empty
+        if (!password) {
+          reject(`No password was provided`);
+        }
+
+        //create the new user
+        const user = new User(email, password, photo, role);
+        UserManager.#all.push(user);
+        resolve(user);
+      } catch (error) {
+        reject(`Error at UserManager.create(): ${error}`);
+      }
+    });
+    return result;
   }
 }

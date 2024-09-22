027335b0254413c436e873ad88bbb3a5eefaad19
julianVillega <julianvillega9@gmail.com>
2024-09-22 15:59:11 -0300

    added middleware for validating user input for users CRUD operations

---
+++ src/middlewares/isValidUser.mid.js
@@ -0,0 +1,55 @@
+import Joi from "joi";
+
+export default function isValidUser(req, res, next) {
+  //define the schema validation
+  const schema = Joi.object({
+    email: Joi.string()
+      .required()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com"] },
+      })
+      .messages({
+        "any.required": "Email is required",
+        "string.email":
+          "Email must follow the pattern xyz@domain.com, only emails ending in .com are supported",
+        "string.empty": "Email cannot be empty",
+      }),
+
+    password: Joi.string()
+      .required()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .messages({
+        "any.required": "Password is required",
+        "string.pattern.base":
+          "Password must be a 3 to 30 characters alphanumeric string",
+        "string.empty": "Password can not be empty",
+      }),
+
+    photo: Joi.string()
+      .uri({ scheme: ["http", "https"], allowRelative: false })
+      .optional()
+      .messages({
+        "string.uri": "Photo must be a valid URL",
+        "string.empty": "If present, photo can not be empty",
+      }),
+
+    role: Joi.string().alphanum().optional().messages({
+      "string.base": "Role must be a string",
+      "string.alphanum": "Role must be alphanumeric",
+      "string.empty": "If present role can not be empty",
+    }),
+  });
+
+  // Validate request body using schema.
+  const { error } = schema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    // Create error and throw it
+    const message = error.details.map((detail) => detail.message).join(", ");
+    const newError = new Error(message);
+    newError.statusCode = 400;
+    return next(newError);
+  }
+  next();
+}
--- src/routers/api/users.api.js
+++ src/routers/api/users.api.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import userControler from "../../controlers/users.controler.js";
+import isValidUser from "../../middlewares/isValidUser.mid.js";
+import setDefaultUserValues from "../../middlewares/setUserDefaultValues.mid.js";
 
 const usersRouter = Router();
 
 usersRouter.get("/", userControler.readAll);
 usersRouter.get("/:id", userControler.readId);
-usersRouter.post("/", userControler.create);
-usersRouter.put("/:id", userControler.update);
+usersRouter.post("/", isValidUser, setDefaultUserValues, userControler.create);
+usersRouter.put("/:id", isValidUser, userControler.update);
 usersRouter.delete("/:id", userControler.delete);
 
 export default usersRouter;

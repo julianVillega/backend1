bb1abcc1fce0d7cc2884d704a29e9c534879fbbc
julianVillega <julianvillega9@gmail.com>
2024-09-22 19:43:03 -0300

    created testUsers.json and testProducts.json, to facilitate testing, do not edit this files

--- src/routers/api/products.api.js
+++ src/routers/api/products.api.js
@@ -2,12 +2,13 @@
 import { Router } from "express";
 import productsControler from "../../controlers/products.controler.js";
 import isValidProduct from "../../middlewares/isValidProduct.mid.js";
+import setDefaultValues from "../../middlewares/setProductDefaultValues.mid.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/", productsControler.readAll);
 productsRouter.get("/:id", productsControler.read);
-productsRouter.post("/", isValidProduct, productsControler.create);
+productsRouter.post("/", isValidProduct, setDefaultValues, productsControler.create);
 productsRouter.put("/:id", isValidProduct, productsControler.update);
 productsRouter.delete("/:id", productsControler.delete);
 

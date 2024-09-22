c60dd56e182d7327a11ed106b3a02dda4c0f5ba7
julianVillega <julianvillega9@gmail.com>
2024-09-22 19:40:46 -0300

    improved validation messages for products and extracted the setting of default values to a new middleware

--- src/middlewares/isValidProduct.mid.js
+++ src/middlewares/isValidProduct.mid.js
@@ -4,26 +4,26 @@ function validator(req, res, next) {
   // Define the schema validation object
   const schema = Joi.object({
     title: Joi.string().required().messages({
-      "string.empty": "Title is required",
-      "string.base": "title must be a string",
-      "any.required": "title is required",
+      "string.empty": "Title can not be empty",
+      "string.base": "Title must be a string",
+      "any.required": "Title is required",
     }),
-    price: Joi.number().positive().required().messages({
-      "number.base": "Price must be a number",
-      "number.positive": "Price must be a positive number",
-      "any.required": "Price is required",
+    price: Joi.number().positive().optional().messages({
+      "number.base": "If present, price must be a number",      
+      "number.positive": "If present, price must be a positive number",
     }),
-    stock: Joi.number().integer().min(0).required().messages({
-      "number.base": "Stock must be a number",
-      "number.integer": "Stock must be an integer",
-      "number.min": "Stock cannot be negative",
-      "any.required": "Stock is required",
+    stock: Joi.number().integer().min(0).optional().messages({
+      "number.base": "If present, stock must be a number",
+      "number.integer": "If present, stock must be an integer",
+      "number.min": "If present, stock cannot be negative",
     }),
     category: Joi.string().optional().messages({
-      "string.base": "Category must be a string",
+      "string.base": "If present, category must be a string",
+      "string.empty":"If present, category can not be empty",
     }),
     photo: Joi.string().uri().optional().messages({
-      "string.uri": "Photo must be a valid URL",
+      "string.uri": "If present, photo must be a valid URL, only http and https protocols are supported",
+      "string.empty": "If present, present, photo can not be empty"
     }),
   });
 
@@ -37,10 +37,6 @@ function validator(req, res, next) {
     newError.statusCode = 400;
     return next(newError);
   }
-
-  // Set category and photo with default values if necessary.
-  !req.body.category && (req.body.category = null)
-  !req.body.photo && (req.body.photo = "https://random.imagecdn.app/200/200")
 
   // Call the next middleware
   next();

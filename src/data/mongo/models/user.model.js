c4db8af410ce8210ddecb93f121d659b65fde70c
Julian Villega <julianvillega9@gmail.com>
2024-10-23 14:03:15 -0300

    fix: role default value was wrong

--- src/data/mongo/models/user.model.js
+++ src/data/mongo/models/user.model.js
@@ -9,7 +9,7 @@ const schema = new Schema({
   },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: 0, enum: ["user", "admin"] },
+  role: { type: String, default: "user", enum: ["user", "admin"] },
   isOnline: { type: Boolean, default: false },
 });
 

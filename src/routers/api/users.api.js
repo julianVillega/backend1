fb42e5de7f4c84ee677e1b92b3b56d6f1aa69be9
Julian Villega <julianvillega9@gmail.com>
2024-10-23 13:24:38 -0300

    switched user.api's fs users controller by mongo's users controller

--- src/routers/api/users.api.js
+++ src/routers/api/users.api.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import userControler from "../../controlers/users.controler.js";
+import userControler from "../../controlers/mongo/users.controler.js";
+// import userControler from "../../controlers/users.controler.js";
 import isValidUser from "../../middlewares/isValidUser.mid.js";
 import setDefaultUserValues from "../../middlewares/setUserDefaultValues.mid.js";
 
 const usersRouter = Router();
 
 usersRouter.get("/", userControler.readAll);
-usersRouter.get("/logout/:id", userControler.logout);
-usersRouter.get("/:id", userControler.readId);
-usersRouter.post("/login", userControler.login);
+// usersRouter.get("/logout/:id", userControler.logout);
+usersRouter.get("/:id", userControler.read);
+// usersRouter.post("/login", userControler.login);
 usersRouter.post("/", isValidUser, setDefaultUserValues, userControler.create);
 usersRouter.put("/:id", isValidUser, userControler.update);
 usersRouter.delete("/:id", userControler.delete);

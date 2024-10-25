67617f20b58c57dcbdae821f52ea5823734246ec
Julian Villega <julianvillega9@gmail.com>
2024-10-25 17:48:18 -0300

    carts controller read method with aggregation to group carts by user and summarize the contents.

--- src/controlers/mongo/carts.controler.js
+++ src/controlers/mongo/carts.controler.js
@@ -4,7 +4,30 @@ import cartsManager from "../../data/mongo/managers/cartsManager.js";
 class CartsContrler extends MongoCrudControler {
   constructor() {
     super(cartsManager, "cart");
+    this.read = this.read.bind(this)
   }
+
+  async read(req, res, next) {
+    try {
+      const { id } = req.params;
+      const instance = await this.manager.read(id);
+      if (instance[0]) {
+        return res.status(200).json({
+          message: `fetched ${this.modelName} from user with id ${id} `,
+          response: instance[0],
+        });
+      } else {
+        const error = new Error(
+          `${this.modelName} for user with id ${id} was not found`
+        );
+        error.statusCode = 404;
+        throw error;
+      }
+    } catch (error) {
+      return next(error);
+    }
+  }
+
 }
 
 const cartsControler = new CartsContrler();
--- src/data/mongo/managers/cartsManager.js
+++ src/data/mongo/managers/cartsManager.js
@@ -1,9 +1,72 @@
+import { Types } from "mongoose";
 import Cart from "../models/cart.model.js";
 import MongoCrudManager from "./MongoCrudManager.js";
 
 class CartsManager extends MongoCrudManager {
   constructor() {
     super(Cart);
+  }
+
+  async read(id) {
+    try {
+      const carts = this.model.aggregate([
+        { $match: { user_id: new Types.ObjectId(id) } },
+  
+        {
+          $lookup: {
+            from: "products",
+            localField: "product_id",
+            foreignField: "_id",
+            as: "productDetails"
+          }
+        },
+        
+        { $unwind: "$productDetails" },
+        
+        { $set: { subTotal: { $multiply: ["$quantity", "$productDetails.price"] } } },
+        
+        {
+          $group: {
+            _id: "$user_id",
+            total: { $sum: "$subTotal" },
+            items: {
+              $push: {
+                _id: "$productDetails._id",
+                category: "$productDetails.category",
+                photo: "$productDetails.photo",
+                title: "$productDetails.title",
+                price: "$productDetails.price",
+                quantity: "$quantity"
+              }
+            }
+          }
+        },
+        
+        {
+          $lookup: {
+            from: "users",
+            localField: "_id",
+            foreignField: "_id",
+            as: "userDetails"
+          }
+        },
+        
+        { $unwind: "$userDetails" },
+        
+        {
+          $project: {
+            _user_id: "$_id",
+            total: 1,
+            items: 1,
+            email: "$userDetails.email",
+            _id: 0
+          }
+        }
+      ]);
+      return carts;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 

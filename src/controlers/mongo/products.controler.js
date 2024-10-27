343f6fbf8a951084ee0f1b60aeac3c6b3b72a7db
Julian Villega <julianvillega9@gmail.com>
2024-10-26 22:01:36 -0300

    added pagination to the productsAdmin view

--- src/controlers/mongo/products.controler.js
+++ src/controlers/mongo/products.controler.js
@@ -43,7 +43,7 @@ class ProductsController extends MongoCrudControler {
         { length: products.totalPages },
         (_, i) => {
           return {
-            link: `http://localhost:8000/?page=${i + 1}&limit=${limit}${
+            link: `http://localhost:8000/?page=${i + 1}&limit=${limit || 5}${
               category ? `&category=${category}` : ""
             }`,
             pageNumber: i + 1,
@@ -51,12 +51,12 @@ class ProductsController extends MongoCrudControler {
         }
       );
       const prevPageLink = products.hasPrevPage
-        ? `http://localhost:8000/?page=${products.prevPage}&limit=${limit}${
+        ? `http://localhost:8000/?page=${products.prevPage}&limit=${limit || 5}${
             category ? `&category=${category}` : ""
           }`
         : "";
       const nextPageLink = products.hasNextPage
-        ? `http://localhost:8000/?page=${products.nextPage}&limit=${limit}${
+        ? `http://localhost:8000/?page=${products.nextPage}&limit=${limit || 5}${
             category ? `&category=${category}` : ""
           }`
         : "";
@@ -85,8 +85,40 @@ class ProductsController extends MongoCrudControler {
 
   async showProductsAdminPannel(req, res, next) {
     try {
-      const products = await this.manager.readAll();
-      return res.render("productsAdmin.handlebars", { products });
+      const { category, limit, page } = req.query;
+      const {userId} = req.params;
+      const products = await this.manager.readAll(
+        category ? { category } : {},
+        { page, limit: limit || 5 }
+      );
+      const pagesLinkArray = Array.from(
+        { length: products.totalPages },
+        (_, i) => {
+          return {
+            link: `http://localhost:8000/products/admin/${userId}?page=${i + 1}&limit=${limit || 5}${
+              category ? `&category=${category}` : ""
+            }`,
+            pageNumber: i + 1,
+          };
+        }
+      );
+      const prevPageLink = products.hasPrevPage
+        ? `http://localhost:8000/products/admin/${userId}?page=${products.prevPage}&limit=${limit || 5}${
+            category ? `&category=${category}` : ""
+          }`
+        : "";
+      const nextPageLink = products.hasNextPage
+        ? `http://localhost:8000/products/admin/${userId}?page=${products.nextPage}&limit=${limit || 5}${
+            category ? `&category=${category}` : ""
+          }`
+        : "";
+
+      return res.render("productsAdmin.handlebars", {
+        products,
+        pagesLinkArray,
+        prevPageLink,
+        nextPageLink,
+      });
     } catch (error) {
       next(error);
     }

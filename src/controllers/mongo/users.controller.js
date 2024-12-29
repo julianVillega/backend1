6048441a673600b5ab202ec51f4168a3476ac241
Julian Villega <julianvillega9@gmail.com>
2024-12-29 17:23:47 -0300

    On branch services Created users service Changes to be committed: 	modified:   src/controllers/mongo/users.controller.js 	new file:   src/services/users.service.js

--- src/controllers/mongo/users.controller.js
+++ src/controllers/mongo/users.controller.js
@@ -1,55 +1,14 @@
 import MongoCrudController from "./mongoCRUD.controller.js";
-import UsersManager from "../../data/mongo/managers/usersManager.js";
-import {createHashUtil} from "../../utils/hash.utils.js"
-
+import usersService from "../../services/users.service.js";
 class UsersController extends MongoCrudController {
   constructor() {
-    super(UsersManager, "user");
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
+    super(usersService, "user");
     this.showUser = this.showUser.bind(this);
     this.showLogin = this.showLogin.bind(this);
     this.showRegister = this.showRegister.bind(this);
     this.create = this.create.bind(this)
   }
 
-  async login(req, res, next) {
-    try {
-      const { email, password } = req.body;
-      const userId = await this.service.login(email, password);
-      if (userId) {
-        console.log(userId);
-        return res
-          .status(200)
-          .json({ message: "login successful", response: userId });
-      } else {
-        const error = new Error();
-        error.statusCode = 401;
-        error.message = "invalid email or password";
-        throw error;
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async logout(req, res, next) {
-    try {
-      const { id } = req.params;
-      const logoutSuccessful = await this.service.logout(id);
-      if (logoutSuccessful) {
-        return res
-          .status(200)
-          .json({ message: "logout successful", response: true });
-      } else {
-        const error = new Error();
-        error.statusCode = 404;
-        error.message = "User not found, logout failed";
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
 
   async showUser(req, res, next) {
     try {
@@ -76,17 +35,7 @@ class UsersController extends MongoCrudController {
       next(error);
     }
   }
-
-  create(req, res, next){
-    try {
-      const {password} = req.body
-      const hashPassword = createHashUtil(password)
-      req.body.password = hashPassword
-      super.create(req, res, next)
-    } catch (error) {
-      next(error)
-    }
-  }
+  
 }
 
 export default new UsersController();

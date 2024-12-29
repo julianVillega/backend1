e276cbdf7771e06347f28a42b9a28abbe91fee02
Julian Villega <julianvillega9@gmail.com>
2024-12-29 15:41:22 -0300

    On branch verifyAccount Created utility for emailing users verification codes. Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/utils/mailing.util.js

---
+++ src/utils/mailing.util.js
@@ -0,0 +1,29 @@
+import { createTransport } from "nodemailer";
+import __dirname from "../../utils.js";
+
+const { GMAIL_EMAIL, GMAIL_PASS } = process.env;
+
+const transport = createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: { user: GMAIL_EMAIL, pass: GMAIL_PASS },
+});
+
+export default async function sendEmail(data) {
+  try {
+    await transport.verify();
+    await transport.sendMail({
+      from: GMAIL_EMAIL,
+      to: data.email,
+      subject: "TiendaTuya Account Verification",
+      html: `<h1>Verify your account<h1>
+      <p>To verify your account, enter the following code on your next login</p>
+      <p>Your verification code is:  ${data.verificationCode}  </p>`,
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+sendEmail();

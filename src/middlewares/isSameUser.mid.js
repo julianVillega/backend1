3c7bf7110e39630bd1be9c5d78a1132899e91e1b
Julian Villega <julianvillega9@gmail.com>
2024-12-29 17:31:28 -0300

    On branch services Fix: is same user mid was not comparing users id's correctly. Changes to be committed: 	modified:   src/middlewares/isSameUser.mid.js

--- src/middlewares/isSameUser.mid.js
+++ src/middlewares/isSameUser.mid.js
@@ -1,6 +1,6 @@
 export default function isSameUser(req, res, next){
   if (req.user.role === "ADMIN") return next();
-  if(req.params.id === req.user._id) return next();
+  if(req.params.id === req.user._id.toString()) return next();
   console.log("Allow User Modification Middleware:Operation Canceled, authed user != target user");
   return res.json403();
 }

2ae5794016ed01377acc51837f715375f2dbbcfa
Julian Villega <julianvillega9@gmail.com>
2025-01-04 11:19:45 -0300

    On branch customRouter Carts view router as customRouter Changes to be committed:   new file:   src/routers/views/carts.view.router.js   modified:   src/routers/views/viewsRouter.js

--- src/routers/views/viewsRouter.js
+++ src/routers/views/viewsRouter.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 
-//mongo based controllers
-import cartsController from "../../controllers/mongo/carts.controller.js";
 // View routers
 import usersRouter from "./users.view.api.js";
 import productsRouter from "./products.view.api.js";
+import cartsRouter from "./carts.view.router.js";
 
 const viewRouter = Router();
 
 viewRouter.use("/users", usersRouter);
 viewRouter.use("/",productsRouter);
-viewRouter.get("/carts/:id", cartsController.showCart);
+viewRouter.use("/carts", cartsRouter);
 export default viewRouter;
